90468622461f6f958c9bd8b16acf685c3a7b2ae9
NishiYusuke <yuusuke8123@gmail.com>
2017-07-20 00:46:11 +0900

    Add files for Arduino,ESP8266,RN4020
    /Arduino101/IoKRead.ino : A program file for Arduino101.
    /ESP8266/IntelHack_ESP8266.ino : A program file for ESP8266 (wifi module).
    RN4020_script.txt : I typed these scripts into RN4020 (Bluetooth module) .

---
+++ Arduino101/IoKRead.ino
@@ -0,0 +1,133 @@
+/*
+I use a sample sketch following this: 
+ - * Sketch: SensortagButton.ino
+ - *
+ - * Description:
+ - *   This Central sketch scan for a Peripheral called the SensorTag.
+ - *   It looks for particular Service, discovers all its attributes,
+ - *   and them on the serial monitor. 
+*/
+
+/*
+ * Copyright (c) 2016 Intel Corporation.  All rights reserved.
+ * See the bottom of this file for the license terms.
+ */
+
+#include <CurieBLE.h>//For Arudino101
+#define Name "IoK" //Set name of a BLE device
+#define ESP_PIN 13//Set a number of output pin for sending data to the Wifi module
+
+void setup() {
+  pinMode(ESP_PIN,OUTPUT);
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  // initialize the BLE hardware
+  BLE.begin();
+
+  Serial.println("BLE Central - IoK Read");
+  Serial.println("Make sure to turn on the device.");
+
+  // start scanning for peripheral
+  BLE.scan();
+}
+
+void loop() {
+  // check if a peripheral has been discovered
+  BLEDevice peripheral = BLE.available();
+
+  if (peripheral) {
+    // discovered a peripheral, print out address, local name, and advertised service
+    Serial.print("Found ");
+    Serial.print(peripheral.address());
+    Serial.print(" '");
+    Serial.print(peripheral.localName());
+    Serial.print("' ");
+    Serial.print(peripheral.advertisedServiceUuid());
+    Serial.println();
+
+    /* see if peripheral is a SensorTag
+     * The localName, CC2650 SensorTag, is in the Scan Response Data packet.
+     * If this is not the expected name, please change the following
+     * if-statement accordingly.
+     */
+    if (peripheral.localName() == "IoK") {
+      // stop scanning
+      BLE.stopScan();
+
+      monitorSensorTagButtons(peripheral);
+
+      // peripheral disconnected, start scanning again
+      BLE.scan();
+    }
+  }
+}
+
+void monitorSensorTagButtons(BLEDevice peripheral)
+{
+  // connect to the peripheral
+  Serial.println("Connecting ...");
+  if (peripheral.connect()) {
+    Serial.println("Connected");
+  } else {
+    Serial.println("Failed to connect!");
+    return;
+  }
+
+  // discover peripheral attributes
+  Serial.println("Discovering attributes of service 0xffe0 ...");
+  if (peripheral.discoverAttributesByService("12345678-9012-3456-7890-1234567890ff")) {
+    Serial.println("Attributes discovered");
+  } else {
+    Serial.println("Attribute discovery failed.");
+    peripheral.disconnect();
+    return;
+  }
+
+  // retrieve the simple key characteristic
+  BLECharacteristic IoKCharacteristic = peripheral.characteristic("12345678-9012-3456-7890-123456789022");
+  
+  while (peripheral.connected()) {
+    // while the peripheral is connected
+
+    // check if the value of the simple key characteristic has been updated
+    // yes, get the value, characteristic is 1 byte so use char value
+    if(IoKCharacteristic.canRead()){
+        IoKCharacteristic.read();
+    }
+     const byte* value = IoKCharacteristic.value();
+     Serial.println(*value);
+     if(*value==0x02){
+       digitalWrite(ESP_PIN, HIGH);
+     }else{
+      digitalWrite(ESP_PIN, LOW);
+     }
+     delay(1000);
+  }
+
+  Serial.println("SensorTag disconnected!");
+}
+
+
+
+/*
+  Arduino BLE Central SensorTag button example
+  Copyright (c) 2016 Arduino LLC. All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+
---
+++ ESP8266/IntelHack_ESP8266.ino
@@ -0,0 +1,83 @@
+#include <WiFiManager.h>
+
+//
+// Copyright 2015 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#include <ESP8266WiFi.h>//https://github.com/esp8266/Arduino
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+
+#include <FirebaseArduino.h>//For using firebase on arduino
+#include <DNSServer.h>
+#define FIREBASE_HOST "******.firebaseio.com"
+//Please set FIREBASE_HOST by the firebase URL like a https://console.firebase.google.com/project/******/overview
+
+String payload;//Status of a key
+const bool  SW= 0;//ESP8266's I/O pin number
+bool  SW_state=0,old_state=0;//Variables for checking door state "Open" or "Lock"
+  
+void setup() {
+  Serial.begin(9600);
+  pinMode(SW,INPUT);
+  // connect to wifi by WiFiManager
+    //Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wifiManager;
+    //reset saved settings
+    wifiManager.resetSettings();
+    
+    //set custom ip for portal
+    //wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+
+    //fetches ssid and pass from eeprom and tries to connect
+    //if it does not connect it starts an access point with the specified name
+    //here  "AutoConnectAP"
+    //and goes into a blocking loop awaiting configuration
+    wifiManager.autoConnect("AutoConnectAP");
+    //or use this for auto generated name ESP + ChipID
+    //wifiManager.autoConnect();
+
+    
+    //if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
+  Firebase.begin(FIREBASE_HOST);
+}
+
+void loop() {
+  SW_state=digitalRead(SW);//Get I/O pin voltage state for checking door state "Open" or "Lock"
+ //-------- Only executed when cheanged door state --------
+ if (old_state!=SW_state) {
+    if(SW_state==HIGH){
+      Serial.println("Lock");
+      payload ="Lock";
+    }else{
+      Serial.println("Open");
+      payload ="Open";
+    }
+    //-------- Indicate "payload"
+    Serial.print("Sending payload: ");
+    Serial.println(payload);
+    // set value
+    Firebase.setString("Status", payload);
+    // handle error
+    if (Firebase.failed()) {
+        Serial.print("setting State:");
+        Serial.println(Firebase.error());  
+        return;
+    }
+    delay(1000);
+ }
+ old_state=SW_state;//Update "old_state"
+}
